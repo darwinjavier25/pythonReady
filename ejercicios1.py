f4538aa9591334596df8fe33b208642388f3d4c3
alexdominguez1 <jalexdgavilanes@gmail.com>
2024-02-27 21:06:29 +0100

    Alex fix

--- ejercicios1.py
+++ ejercicios1.py
@@ -1,3 +1,12 @@
 #escribir un codigo con dos variables que sume dos datos
+Dato = 23
+Minidato = 12
+print(Dato+Minidato)
 #escribir un codigo que pinte nombre y apellido siendo nombre y apellido dos variables
+nombre = "alex"
+apellido = " dominguez"
+print (nombre + apellido)
 #escribir una lista de 5 elementos
+lista = ["casa",2 , 5.5, False, (8, 9, 1)]
+git(commit)
+gitpush
