1dc804303f567cd00273446c04772d6df5cbcaa1
alexdominguez1 <jalexdgavilanes@gmail.com>
2024-02-27 21:43:15 +0100

    s

---
+++ ejercicios.py
@@ -0,0 +1,20 @@
+#escribir un codigo con dos variables que sume dos datos
+Dato = 23
+Minidato = 12
+print(Dato+Minidato)
+#Respuesta ideal. Crear una variable para guardar la suma, por ejemplo
+result = Dato+Minidato
+print(result)
+#escribir un codigo que pinte nombre y apellido siendo nombre y apellido dos variables
+nombre = "alex"
+apellido = " dominguez"
+print (nombre + apellido)
+#bastante bien
+#escribir una lista de 5 elementos
+lista = ["casa",2 , 5.5, False, (8, 9, 1)]
+#Bastante bie
+#Lo ideal para pintar la lista en python es crear un bucle for
+#El for guarda la lista en i y el in lista le dice que guardar, el print pinta la variable i que produce el for
+for i in lista:
+    print(i)
+#fin codigo
